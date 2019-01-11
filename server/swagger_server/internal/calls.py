4de9d7c60fc16f61d81fb3c87e9a769d2662d0fe
kakwa <carpentier.pf@gmail.com>
2019-01-11 12:24:01 +0100

    adding the actual calls skeleton

---
+++ server/swagger_server/internal/calls.py
@@ -0,0 +1,59 @@
+def _account_create(body):
+    pass
+
+def _account_delete(accountId):
+    pass
+
+def _account_get(accountId):
+    pass
+
+def _account_list(next_id=None):
+    pass
+
+def _account_update(accountId, body):
+    pass
+
+def _apikey_create(accountId):
+    pass
+
+def _apikey_delete(accountId, keyId):
+    pass
+
+def _apikey_get(accountId, keyId):
+    pass
+
+def _apikey_list(accountId, next_id=None):
+    pass
+
+def _certificate_create(domainName, body):
+    pass
+
+def _certificate_delete(domainName, certificateId):
+    pass
+
+def _certificate_get(domainName, certificateId):
+    pass
+
+def _certificate_list(domainName, next_id=None):
+    pass
+
+def _domain_create(body):
+    pass
+
+def _domain_delete(domainName):
+    pass
+
+def _domain_get(domainName):
+    pass
+
+def _domain_list(next_id=None):
+    pass
+
+def _notification_acknowledge(notificationId, body):
+    pass
+
+def _notification_get(notificationId):
+    pass
+
+def _notification_list(next_id=None):
+    pass
